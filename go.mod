9ddb6176bbd34c3ce53a41f3b094217a2bb74a20
wsChendp <2547825462@qq.com>
2025-02-28 15:36:31 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/cloudwego/biz-demo/gomall/demo/demo_thrift
+
+go 1.23.1
---
+++ idl/new_echo.thrift
@@ -0,0 +1,15 @@
+namespace go api
+
+struct Request {
+    1: string message
+}
+
+struct Response {
+    1: string message
+}
+
+service Echo {
+    Response echo(1: Request request)
+}
+
+
